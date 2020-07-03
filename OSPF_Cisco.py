7f0f1466f2a16eae1a1fd4027133caeec9218b1b
Morteza <44939554+Mozart4242@users.noreply.github.com>
2020-07-03 17:57:58 +0430

    Add files via upload
    Requisite: Python3

---
+++ OSPF_Cisco.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import getpass
+import telnetlib
+
+#OSPF
+def ospf(HOST,user,password,ensec):
+	nw = input("In which network do you want to run OSPF? >> ")
+	wildmask = input("Enter Wildcard mask: >> ")
+	pid = input("Proccess id? >> ")
+	areaid = input("What Area? >> ")
+	tn = telnetlib.Telnet(HOST)
+
+	tn.read_until(b"Username: ")
+	tn.write(user.encode('ascii') + b"\n")
+	if password:
+	    tn.read_until(b"Password: ")
+	    tn.write(password.encode('ascii') + b"\n")
+
+	tn.write(b"en\n")
+	tn.write(ensec.encode('ascii') + b"\n")
+	tn.write(b"conf t\n")
+	tn.write(b"router ospf " + pid.encode('ascii') + b"\n")
+	tn.write(b"network " + nw.encode('ascii') + b" " + wildmask.encode('ascii') + b" area " + areaid.encode('ascii') + b"\n")
+	tn.write(b"end\n")
+	tn.write(b"exit\n")
+
+	print(tn.read_all().decode('ascii'))
+	
+	
+#START
+print("what do you want to Configure?")
+list1 = ["OSPF","Vlan","Trunk","STP"]
+list2 = []
+n = 1
+for i in list1 :
+	list2.append(n)
+	print(n,":",i)
+	n = n + 1
+
+while True:
+	select = int(input("choose a number: "))
+	if select in list2:
+		HOST = input("Enter your Device IP Addrees >> ")
+		user = input("Enter your remote account: >> ")
+		password = getpass.getpass()
+		ensec = input("Enter your password/secret: (leave empty if not defined) >> ")
+		print("OK, Lets Configure...")
+
+		if select == 1:
+			ospf(HOST,user,password,ensec)
+		elif select == 2:
+			Vlan()
+		elif select == 3:
+			Trunk()
+		elif select == 4:
+			STP()
+		exit(0)
+	else:
+		print("Please Enter the Operation Number of the list")
+		
+
